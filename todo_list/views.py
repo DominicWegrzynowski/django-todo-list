a42116240c318f3af846fbc18cbef0dfb2541bc3
Dominic Wegrzynowski <dominicwegrzynowski@gmail.com>
2020-12-07 11:36:47 -0500

    Running first migrations, creating admin, creating templates/views

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.tempalte import loader
-from todo_list.models import Item 
+from django.template import loader 
 
 # Create your views here.
+def index(request):
+    return render(request, 'todolist/todolist.html')
